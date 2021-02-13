f20ea173eb9c35d124b344bb665c15a3203dea8e
koukobashi <56275354+tyxiang0530@users.noreply.github.com>
2021-02-13 15:49:01 +0800

    hw2

---
+++ module_2.ino
@@ -0,0 +1,30 @@
+    /*
+      Module 2 sketch 
+      @author Tai Xiang
+      module that turns LED ON if sensorValue over 512ms, off if otherwise
+     */
+     
+    // designate pins
+    const int analogInPin = A0;
+    int led = 5;
+     
+    void setup() {                
+      // initialize pins and serial monitor
+      pinMode(led, OUTPUT);
+      Serial.begin(9600);
+    }
+     
+    // the loop routine runs over and over again forever:
+    void loop() {
+      if (analogRead(analogInPin) > 512){
+        // sensorvalue is high
+        Serial.print("LED ON");
+        digitalWrite(led, HIGH);   // turn LED on
+      }
+
+      else{
+        // sensorvalue is low
+        Serial.print("LED OFF");
+        digitalWrite(led, LOW);    // turn LED off
+      }
+    }
