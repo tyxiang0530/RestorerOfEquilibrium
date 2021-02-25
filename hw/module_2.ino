73e9400a68ec34aa84431c2c9d99b48b73fa50ab
koukobashi <56275354+tyxiang0530@users.noreply.github.com>
2021-02-25 21:53:10 +0800

    Update module_2.ino

--- hw/module_2.ino
+++ hw/module_2.ino
@@ -14,7 +14,6 @@
       Serial.begin(9600);
     }
      
-    // the loop routine runs over and over again forever:
     void loop() {
       if (analogRead(analogInPin) > 512){
         // sensorvalue is high
