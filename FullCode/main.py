312b59b3e321be423c4637dcb5e9f96e19fe6637
koukobashi <56275354+tyxiang0530@users.noreply.github.com>
2021-05-13 10:27:04 +0800

    Update main.py

--- FullCode/main.py
+++ FullCode/main.py
@@ -1,12 +1,6 @@
-import ktrain
 import tensorflow as tf
-import random
-import serial
-import cv2
 import numpy as np
-import serial
 from time import sleep
-from playsound import playsound
 from ProcessSpeech import play_prompt, reaction, listen_for
 from DetectPlay import play_game
 
@@ -17,8 +11,6 @@ def main():
     play_prompt("audio\\init.mp3")
     # arduino = serial.Serial(port='COM7', baudrate=9600, timeout=.1)
     # send byte that will activate on off flashing of LED for arduino
-    # load emo model
-    emo_predictor = ktrain.load_predictor("tf_model.h5")
     # load rps model
     rps_predict = tf.keras.models.load_model("rock_paper_scissors_cnn.h5")
     shape_to_label = {b'r':np.array([1.,0.,0.]), b'p':np.array([0.,1.,0.]), b's':np.array([0.,0.,1.])}
@@ -30,8 +22,9 @@ def main():
 
     # run emotional prompts
     emo_prompt = "audio\\emoprompt.mp3"
-    reaction(emo_predictor, emo_prompt)
+    reaction(emo_prompt)
 
+    sleep(2)
     # prompt user to play game
     game_prompt = "audio\\game_prompt.mp3"
     reject_prompt = "audio\\no.mp3"
